4726fc0a9a23fe9983654529f1e35ede85225557
Denis Kent <denis.kent@snyk.io>
2025-03-18 13:23:01 +0000

    feat: added typescript

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.del("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
