66b973261559eaaa269a40e793128e873f951aab
Denis Kent <denis.kent@snyk.io>
2025-03-18 15:12:46 +0000

    chore: run

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,14 @@ import * as express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.param("id", function (req, res, next, id) {
+  console.log("app.param is called");
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
+  const searchParam = req.param("search");
+  res.send("Express + TypeScript Server" + searchParam);
 });
 
 app.listen(port, () => {
