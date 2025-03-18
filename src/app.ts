0c8d9ea93a37d3b08c099f689936de25c5b60c81
Denis Kent <denis.kent@snyk.io>
2025-03-18 14:23:58 +0000

    chore: got working app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
 import * as express from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Express + TypeScript Server");
 });
 
-// app.del("/", (req, res) => {
-//   res.send("Hello World");
-// });
-
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
