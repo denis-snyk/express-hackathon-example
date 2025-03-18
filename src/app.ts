f7209a610242316c0eb99a3df48a7579078673f8
Denis Kent <denis.kent@snyk.io>
2025-03-18 13:23:16 +0000

    chore: fixed issue

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.del("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.del("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
